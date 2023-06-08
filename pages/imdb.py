873b8a5af782360ab23cebf5baf6e77b6de22f4f
kef <kekekefir@gmail.com>
2023-06-08 12:49:54 +0300

    branch test

---
+++ pages/imdb.py
@@ -0,0 +1,21 @@
+import os
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import re
+import string
+from collections import Counter
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+
+from sklearn.model_selection import train_test_split
+
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn as nn
+import torchutils as tu
+from torchmetrics.classification import BinaryAccuracy
+
+# 4324324234324234324
